b6dafca40a35038fdd821ef17cb485a63fa56798
Christian Scheer <christianscheer84@gmail.com>
2021-05-01 10:57:52 +0200

    maven setup

---
+++ selenium/src/main/java/tech/cscheer/impfen/selenium/App.java
@@ -0,0 +1,10 @@
+package tech.cscheer.impfen.selenium;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
