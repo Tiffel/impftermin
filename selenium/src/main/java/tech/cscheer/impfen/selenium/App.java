0231fb4fd07c1413e6aba02d7e731808389368dc
Christian Scheer <christianscheer84@gmail.com>
2021-05-02 16:33:04 +0200

    jar paketieren. optionsn für chrome in linux container

--- selenium/src/main/java/tech/cscheer/impfen/selenium/App.java
+++ selenium/src/main/java/tech/cscheer/impfen/selenium/App.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import tech.cscheer.impfen.selenium.page.AktionsauswahlPage;
@@ -19,7 +20,11 @@ public class App {
 
     public static void main(String[] args) {
         WebDriverManager.chromiumdriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--verbose");
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        WebDriver driver = new ChromeDriver(options);
         Wait<WebDriver> wait = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(30))
                 .pollingEvery(Duration.ofSeconds(5))
