4451e97ea5d4e978cb9da7a4c512a03a58d91330
Christian Scheer <christianscheer84@gmail.com>
2021-05-03 22:56:56 +0200

    endlosschleife in ordenltlich

--- selenium/src/main/java/tech/cscheer/impfen/selenium/App.java
+++ selenium/src/main/java/tech/cscheer/impfen/selenium/App.java
@@ -1,21 +1,12 @@
 package tech.cscheer.impfen.selenium;
 
-import static tech.cscheer.impfen.selenium.Environment.PORTAL_PASSWORD;
-import static tech.cscheer.impfen.selenium.Environment.PORTAL_USERNAME;
-import static tech.cscheer.impfen.selenium.Environment.VACCINATION_CENTERS;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.util.stream.IntStream;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import tech.cscheer.impfen.selenium.page.AktionsauswahlPage;
 import tech.cscheer.impfen.selenium.page.Impfzentrum;
 import tech.cscheer.impfen.selenium.page.LandingPage;
@@ -23,11 +14,19 @@ import tech.cscheer.impfen.selenium.page.TerminfindungPage;
 import tech.cscheer.impfen.selenium.page.TerminvergabePage;
 import tech.cscheer.impfen.selenium.page.ZugangPage;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+import static tech.cscheer.impfen.selenium.Environment.PORTAL_PASSWORD;
+import static tech.cscheer.impfen.selenium.Environment.PORTAL_USERNAME;
+import static tech.cscheer.impfen.selenium.Environment.VACCINATION_CENTERS;
+
 public class App {
     private static final long SLEEP_TIME_LEFT_LIMIT = Duration.ofMinutes(4).toMillis();
     private static final long SLEEP_TIME_RIGHT_LIMIT = Duration.ofMinutes(6).toMillis();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Environment.init();
         WebDriverManager.chromiumdriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -49,22 +48,23 @@ public class App {
         ZugangPage.handle(driver, wait, PORTAL_USERNAME, PORTAL_PASSWORD);
         AktionsauswahlPage.handle(driver, wait);
 
-        for (int i = 0; i < VACCINATION_CENTERS.size(); i++) {
-            // Gerüchten zufolge ist die "Ab" Suche der Webseite kaputt, deswegen suchen als "ab" in den nächsten 2 Wochen
-            LocalDate today = LocalDate.now();
-            Impfzentrum impfzentrum = VACCINATION_CENTERS.get(i);
-            IntStream.range(1, 15).forEach(day -> {
-                LocalDate datum = today.plusDays(day);
-                TerminfindungPage.handle(driver, wait, impfzentrum, datum);
-                TerminvergabePage.handle(driver, wait);
-            });
-
-            // Endlosschleife
-            if (i == VACCINATION_CENTERS.size() - 1) {
-                i = -1;
+        while (true) {
+            for (Impfzentrum impfzentrum : VACCINATION_CENTERS) {
+                // Gerüchten zufolge ist die "Ab" Suche der Webseite kaputt, deswegen suchen als "ab" in den nächsten 2 Wochen
+                LocalDate today = LocalDate.now();
+                IntStream.range(1, 15).forEach(day -> {
+                    LocalDate datum = today.plusDays(day);
+                    TerminfindungPage.handle(driver, wait, impfzentrum, datum);
+                    TerminvergabePage.handle(driver, wait);
+                });
+            }
+            try {
                 Thread.sleep(randomSleepTime());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
+
     }
 
     private static long randomSleepTime() {
