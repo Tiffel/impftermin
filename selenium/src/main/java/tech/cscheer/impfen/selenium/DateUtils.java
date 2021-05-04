b1cb538008a7c4869db53b59475036e4e5b664a5
i7i5 <extranull@posteo.de>
2021-05-04 18:30:34 +0200

    Lambdas <3

--- selenium/src/main/java/tech/cscheer/impfen/selenium/DateUtils.java
+++ selenium/src/main/java/tech/cscheer/impfen/selenium/DateUtils.java
@@ -2,19 +2,15 @@ package tech.cscheer.impfen.selenium;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DateUtils {
     public static List<ZonedDateTime> getNextTwoWeeksToCheck() {
-        List<ZonedDateTime> nextTwoWeeks = new ArrayList<>();
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/Berlin"));
-        IntStream.range(1, 15).forEach(day -> {
-            ZonedDateTime datum = now.plusDays(day);
-            nextTwoWeeks.add(datum);
-        });
-
-        return nextTwoWeeks;
+        return IntStream.range(1, 15)
+                .mapToObj(now::plusDays)
+                .collect(Collectors.toList());
     }
 }
